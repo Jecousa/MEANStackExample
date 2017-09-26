cf9d2c2b92c11179bd692b9c6094f872b928c5ee
Jakob Essig <jecolousa@gmail.com>
2017-09-26 08:12:23 -0600

    first Commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+// Connect to DB
+mongoose.connect(config.database);
+
+
+//Connect MEssage
+mongoose.connection.on('connected', () => {
+  console.log('Connected to database '+config.database);
+});
+
+//DB Error
+mongoose.connection.on('error', (err) =>{
+  console.log('database error: ' + err);
+});
+
+const app = express();
+
+const users = require('./routes/users');
+
+//Port #
+const port = 3002;
+
+app.use(cors());
+
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Body Parser Minddleware
+
+app.use(bodyParser.json());
+
+//Body Parser Minddleware
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+ require('./config/passport')(passport);
+
+app.use('/users', users);
+
+//Index Route
+app.get('/', (req, res) => {
+  res.send('sanity check!');
+});
+
+
+//Start Server
+app.listen(port, () => {
+  console.log('Server Started' + port);
+});
